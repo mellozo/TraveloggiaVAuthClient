328ab3550bc3e286387932922ab9461f36381377
melozzo <acapsis@AccademiaDegliIncogniti.net>
2016-03-10 16:32:25 -0800

    confirm cancel directive - works w/wout compile version, but neither inside of google maps info window -todo: try ng-include

---
+++ source/common/ConfirmCancelDirective.js
@@ -0,0 +1,28 @@
+﻿angularTraveloggia.directive('confirmCancel', function ($compile) {
+    return {
+        restrict: 'E',
+
+     //   templateUrl: "common/confirm-cancel.html",
+        link: function (scope, element, attrs) 
+        {
+
+            scope.question = attrs.question;
+
+            element.find('#btnConfirm').bind('click',function(event){
+                scope.$eval(attrs.confirm)
+            })
+
+            element.find('#btnCancel').bind('click', function (event) {
+                scope.$eval(attrs.cancel)
+            })
+
+          //  var placeholder =   angular.element(document.getElementById('confirm-cancel-placeholder'));
+            var dhtml = '<div class="container" style="width:200px"><div class="panel panel-default"><div class="panel-body">'+
+      '<p><span style="font:Centaur;color:brown">{{question}}</span></p><button type="button" class="btn btn-primary" id="btnConfirm">Yes</button>'+
+      '<button type="button" class="btn btn-danger" id="btnCancel"  >No</button></div></div>'
+                 element.append( $compile(dhtml) (scope));
+        
+        }//end link
+    }// end return directive
+
+})
