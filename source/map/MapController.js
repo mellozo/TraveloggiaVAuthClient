7259fc2a2f3c6fb60e612312f0276d1f5b23dc95
melozzo <acapsis@AccademiaDegliIncogniti.net>
2016-09-23 13:58:41 -0700

    fix map loading bug

--- source/map/MapController.js
+++ source/map/MapController.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="Map.html" />
+﻿
 angularTraveloggia.controller('MapController', function (SharedStateService, canEdit, readOnly, isEditing, $window, $route, $scope, $location, DataTransportService, $timeout, $http, debounce) {
 
 
@@ -21,14 +21,12 @@ angularTraveloggia.controller('MapController', function (SharedStateService, can
     // INIT SEQUENCE
 
     var clearSites = function () {
-
         for (var i = 0; i<$scope.mapInstance.entities.getLength() ; i++) {
             var pushpin = $scope.mapInstance.entities.get(i);
             if (pushpin instanceof Microsoft.Maps.Pushpin) {
                 $scope.mapInstance.entities.removeAt(i);
             }
         }
-
     }
 
 
@@ -111,8 +109,7 @@ angularTraveloggia.controller('MapController', function (SharedStateService, can
                     SharedStateService.setSelected("Site", null);// clear any previous settings
                     if ($scope.MapRecord.Sites.length > 0) {
                         SharedStateService.Repository.put("Sites", $scope.MapRecord.Sites);
-                        drawSites();
-                        
+                        drawSites();                        
                     }
                     else
                         $scope.systemMessage.loadComplete = true;
@@ -155,20 +152,15 @@ angularTraveloggia.controller('MapController', function (SharedStateService, can
 
                 $scope.MapRecord = result.data;
                 SharedStateService.Repository.put('Map', result.data);
+                SharedStateService.setSelected("Site", null)
+                clearSites();
              
-              
-                    SharedStateService.setSelected("Site", null)
-                 
-                    $timeout(function () {
-                        clearSites();
-                    },3000)
           
               
                 if ($scope.MapRecord.Sites.length > 0)
-                  
+                    $timeout(function () {
                         drawSites();
-                
-                    
+                    },1000) 
                 else
                     $scope.systemMessage.loadComplete = true;
 
@@ -256,7 +248,7 @@ angularTraveloggia.controller('MapController', function (SharedStateService, can
               return SharedStateService.Selected.Site.SiteID;
       },
       function (newValue, oldValue) {
-          if (newValue != null && newValue != oldValue)
+          if (newValue != null && newValue != "null" && newValue != oldValue)
           {
               var searchObject = $location.search()
               if (($location.path() != "/MapList") && searchObject["ZoomIn"] == "true")
@@ -283,7 +275,7 @@ angularTraveloggia.controller('MapController', function (SharedStateService, can
               return SharedStateService.Selected.Map.MapID;
       },
       function (newValue, oldValue) {
-          if (newValue != null && newValue != oldValue) {
+          if ($location.path() == "/MapList" && newValue != null && newValue != oldValue) {
               loadSelectedMap()
           }
 
