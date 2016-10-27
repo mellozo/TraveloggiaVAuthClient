21c175ca513fc91b9100c175ad46744f8e4617f0
melozzo <acapsis@AccademiaDegliIncogniti.net>
2016-10-26 18:44:36 -0700

    even sandeep panda would approve

--- source/common/DateTimeWrapperDirective.js
+++ source/common/DateTimeWrapperDirective.js
@@ -6,29 +6,26 @@
         scope: {
             mel: '='
         },
+      replace:true,
         template:
-                 "<div class='input-group date'  id='dtPicker'>" +
+                 "<div class='input-group date'  >" +
                     "<input   id='dateInputField'   type='text' class='form-control'   />"+
                     "<span class='input-group-addon'>" +
                         "<span class='glyphicon glyphicon-calendar'></span>"+
                     "</span>"+
                 "</div>",
         link: function (scope, elem, attrs, ngModel) {
-
-            $("#dtPicker").datetimepicker();
-           var picker = $('#dtPicker').data('DateTimePicker');
+            var selector = "#" + attrs.id;
+            $('#'+attrs.id).datetimepicker();
+            var picker = $('#' + attrs.elid).data('DateTimePicker');
             var ipt = elem.find("#dateInputField");
             ipt[0].value = scope.mel;
                ipt.on('blur', function () {
-                  // scope.mel = ipt[0].value;
                    scope.$apply(function() {
                        ngModel.$setViewValue(ipt[0].value);
                        });
-                  
                })
-
-
-
+            
         }
 
     }
