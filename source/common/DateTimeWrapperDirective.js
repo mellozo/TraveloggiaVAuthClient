22857d41c4b78c57a5fd46c34159c917d8570a40
melozzo <acapsis@AccademiaDegliIncogniti.net>
2016-10-26 17:17:36 -0700

    working datetimepicker directive - will try to improve

---
+++ source/common/DateTimeWrapperDirective.js
@@ -0,0 +1,35 @@
+﻿angularTraveloggia.directive("wrappedDate", function () {
+    return {
+
+        restrict: "E",
+        require:'ngModel',
+        scope: {
+            mel: '='
+        },
+        template:
+                 "<div class='input-group date'  id='dtPicker'>" +
+                    "<input   id='dateInputField'   type='text' class='form-control'   />"+
+                    "<span class='input-group-addon'>" +
+                        "<span class='glyphicon glyphicon-calendar'></span>"+
+                    "</span>"+
+                "</div>",
+        link: function (scope, elem, attrs, ngModel) {
+
+            $("#dtPicker").datetimepicker();
+           var picker = $('#dtPicker').data('DateTimePicker');
+            var ipt = elem.find("#dateInputField");
+            ipt[0].value = scope.mel;
+               ipt.on('blur', function () {
+                  // scope.mel = ipt[0].value;
+                   scope.$apply(function() {
+                       ngModel.$setViewValue(ipt[0].value);
+                       });
+                  
+               })
+
+
+
+        }
+
+    }
+});
